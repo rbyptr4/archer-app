85b7ee2ce2739b704462146d39b5acec13a4b1f9
Roby Rantung <roby.rantung25@gmail.com>
2025-11-07 23:24:37 +0700

    Fix bug

--- routers/paymentRoutes.js
+++ routers/paymentRoutes.js
@@ -7,10 +7,14 @@ const wh = require('../controllers/payments/xenditWebhookController');
 // in-app start session
 router.get('/status/:id', ctrl.getSessionStatus);
 
-router.post(
-  '/webhooks/xendit',
-  express.json({ type: '*/*' }),
-  wh.xenditQrisWebhook
-);
+router.post('/webhooks/xendit', express.raw({ type: '*/*' }), (req, res) => {
+  try {
+    const body = JSON.parse(req.body.toString());
+    wh.xenditQrisWebhook(body, req, res);
+  } catch (e) {
+    console.error('[Webhook raw parse failed]', e);
+    res.status(400).json({ message: 'Bad JSON' });
+  }
+});
 
 module.exports = router;
